e7e9cab892c76ecb10c14e55729ff13a71eef153
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-05 12:20:38 -0400

    Add morgan to app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 import express from "express";
 const app = express();
 export default app;
+
+import morgan from "morgan";
+
+import tracksRouter from "#api/tracks";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use("/tracks", tracksRouter);
