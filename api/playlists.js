cd31ab0a0efa22a9dc631e0d0ba223d54ebe69d0
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-05 13:04:59 -0400

    Add POST /playlists route

--- api/playlists.js
+++ api/playlists.js
@@ -2,9 +2,18 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { getPlaylists } from "#db/queries/playlists";
+import { getPlaylists, createPlaylist } from "#db/queries/playlists";
 
 router.get("/", async (req, res) => {
   const playlists = await getPlaylists();
   res.send(playlists);
+});
+
+router.post("/", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request must have a body");
+  const { name, description } = req.body;
+  if (!name || !description)
+    return res.status(400).send("Request requires name and description");
+  const playlist = await createPlaylist(name, description);
+  res.send(playlist);
 });
